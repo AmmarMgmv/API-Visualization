80c48577a19eeebbcf44a14674727371f1c920c5
AmmarMgmv <61151686+AmmarMgmv@users.noreply.github.com>
2021-01-05 23:56:07 +0000

    simple doughnut chart for languages

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 function handleInput() {
-    var username = document.getElementById("username").value;
-    var auth = document.getElementById("auth").value !== "" ? document.getElementById("auth").value : undefined;
-    main(username, auth);
+    var user = document.getElementById("user").value;
+
+    main(user);
 }
 
-async function getRequest(url, auth) {
-    const headers = {
-        'Authorization': `Token ${auth}`
+async function getRequest(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+async function main(user) {
+    let url = `https://api.github.com/users/${user}/repos`;
+    let repo = await getRequest(url).catch(error => console.error(error));
+
+    language(repo, user);
+}
+
+async function language(repo, user) {
+    let label = [];
+    let data = [];
+    let backgroundColor = [];
+
+    for (i in repo) {
+        let url = `https://api.github.com/repos/${user}/${repo[i].name}/languages`;
+        let languages = await getRequest(url).catch(error => console.error(error));
+
+        for (language in languages) {
+
+            if (label.includes(language)) {
+                for (j = 0; j < label.length; j++)
+                    if (language == label[i])
+                        data[j] = data[i] + languages[language];
+
+            } else {
+                label.push(language);
+                data.push(languages[language]);
+                backgroundColor.push(`rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.2)`);
+            }
+        }
+
     }
-    const response = (auth == undefined) ? await fetch(url) : await fetch(url, {
-        "method": "GET",
-        "headers": headers
-    });
+
+    draw('doughnutChart', 'doughnut', 'languages', 'Languages Used', label, data, backgroundColor);
+
 }
 
-async function main(username, auth) {
-    let url = `https://api.github.com/users/${username}/repos`;
-    let repos = await getRequest(url, auth).catch(error => console.error(error));
+function draw(ctx, type, datasetLabel, titleText, label, data, backgroundColor) {
+
+    new Chart(doughnutChart, {
+        type: type,
+        data: {
+            labels: label,
+            datasets: [{
+                label: datasetLabel,
+                data: data,
+                backgroundColor: backgroundColor,
+                borderWidth: 1,
+                borderColor: '#777',
+                hoverBorderWidth: 2,
+                hoverBorderColor: '#000'
+            }],
+
+        },
+    });
 }
